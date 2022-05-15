970a2a0ddd496fe557a3551b5f01170a785e4a7c
Lee SJ <ei@genshin.ai>
2022-05-16 01:12:02 +0900

    no message

--- DailyCheck_Jandi.js
+++ DailyCheck_Jandi.js
@@ -1,13 +1,16 @@
 function Current_Date() {
-    const now = new Date();
-    const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
-    const koreaTimeDiff = 9 * 60 * 60 * 1000;
-    const koreaNow = new Date(utcNow + koreaTimeDiff);
+    const Now = new Date();
+    const Utc_Now = Now.getTime() + (Now.getTimezoneOffset() * 60 * 1000);
+    const Korea_Time_Diff = 9 * 60 * 60 * 1000;
+    const Korea_Time_Now = new Date(Utc_Now + Korea_Time_Diff);
 
-    return [koreaNow.getFullYear(), koreaNow.getMonth() + 1, koreaNow.getDate()];
+    return [Korea_Time_Now.getFullYear(), Korea_Time_Now.getMonth() + 1, Korea_Time_Now.getDate()];
 }
 
 const current_date = Current_Date();
+const Jandi = [[], [], [], [], [], [], []]
 for (let i = 0; i < new Date(current_date[0], current_date[1], 0).getDate(); i++) {
-    console.log(`${current_date[0]}년 ${current_date[1].toString().padStart(2, '0')}월 ${(i + 1).toString().padStart(2, '0')}일 ${parseInt((0b1110111110111011110011111101011 >> i & 0b1).toString(), 2)}`);
+    Jandi[i % 7].push((83748 >> i & 0b1) ? '🟩' : '⬜'); // 0b1110111110111010110011111101011
+    // console.log(`${current_date[0]}년 ${current_date[1].toString().padStart(2, '0')}월 ${(i + 1).toString().padStart(2, '0')}일 ${(0b1110111110111011110011111101011 >> i & 0b1) ? '🟩' : '⬜'}`);
 }
+console.log(Jandi.join('\n'));
